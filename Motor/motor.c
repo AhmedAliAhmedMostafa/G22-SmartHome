07b6497862f3023b1e95404e93c1361f5ba645ce
AhmedAliAhmedMostafa <ahmed_elady42@yahoo.com>
2019-04-29 14:21:58 +0200

    Add Implementation OF Rotate Functions .

---
+++ Motor/motor.c
@@ -0,0 +1,22 @@
+#include"motor.h"
+void Rotate(Rotation_Dirction dir,uint32_t degree)
+{
+  uint16_t num_steps;
+	inti,j;
+	num_steps=degree/motor_step;
+  for(j=0;j<num_steps;j++)
+	{	
+		for(i=0;i<4;i++)
+		{
+			if(dir==psitive)
+			{
+				GPIO_PORTX_DATA_R=(GPIO_PORTX_DATA_R&~Port_mask)|sequence[4-i];
+			}
+			else if(dir==negative)
+			{
+				GPIO_PORTX_DATA_R=(GPIO_PORTX_DATA_R&~Port_mask)|sequence[i];
+			}
+			/*wait some time depend on the motor data sheet*/
+		}
+	}
+}
