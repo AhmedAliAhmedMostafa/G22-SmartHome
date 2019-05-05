b32fe67da34d3aa599e6eb814eddd6b7649d5fa7
shimaa-mahmoud <shamiaa220@gmail.com>
2019-05-05 04:55:34 +0200

    Update GPIO_HANDLER.h

--- GPIO ports/GPIO_HANDLER.h
+++ GPIO ports/GPIO_HANDLER.h
@@ -1,4 +1,5 @@
 #include"TM4C123GH6PM.h"
+#include"motor.h"
 void GPIOF_HANDLER(void)		// INTERNAL SWITCHES ARE AT ---PF1--- AND ---PF4---
 {
 	volatile int readback;
@@ -7,12 +8,20 @@ void GPIOF_HANDLER(void)		// INTERNAL SWITCHES ARE AT ---PF1--- AND ---PF4---
 		if (GPIOF->MIS & 0x10) /* is it SW1(PF4)? */
 		{
 			/* CODE TO WRITE ON UART_DR REGISTER ex: UART0->DR = '$' WHERE THIS SIGN '$' MEANS +30 DEG AT MOTOR SIDE  */
+			
+			// make sure that the port is identified in motor.c 
+			Rotate(negative,30);
+			
 			GPIOF->ICR |= 0x10;			 /* clear the interrupt flag */
 			readback = GPIOF->ICR; 	/* a read to force clearing of interrupt*/
 		}
 		else if (GPIOF->MIS & 0x01) /* then it must be SW2(PF0) */
 		{
 			/* CODE TO WRITE ON UART_DR REGISTER ex: UART0->DR = '#' WHERE THIS SIGN '#' MEANS -30 DEG AT MOTOR SIDE  */
+			
+			// make sure that the port is identified in motor.c 
+			Rotate(positive,30);
+			
 			GPIOF->ICR |= 0x10;			 /* clear the interrupt flag */
 			readback = GPIOF->ICR; 	/* a read to force clearing of interrupt*/
 		}
